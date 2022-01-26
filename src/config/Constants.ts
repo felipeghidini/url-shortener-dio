5ed4f56687d8e010100a59d6361334fcbf73b9c5
felipeghidini <lipeghidini18@gmail.com>
2022-01-25 21:53:42 -0300

    Projeto finalizado

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://felipe:felipe@url-shotener-dio.7mzur.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
---
+++ src/controller/URLController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express'
+import shortId from 'shortid'
+import { config } from '../config/Constants'
+import { URLModel } from '../database/model/URL'
+
+export class URLController {
+	public async shorten(req: Request, response: Response): Promise<void> {
+		const { originURL } = req.body
+		const url = await URLModel.findOne({ originURL })
+		if (url) {
+			response.json(url)
+			return
+		}
+		const hash = shortId.generate()
+		const shortURL = `${config.API_URL}/${hash}`
+		const newURL = await URLModel.create({ hash, shortURL, originURL })
+		response.json(newURL)
+	}
+
+	public async redirect(req: Request, response: Response): Promise<void> {
+        // Pegar o hash da URL
+        // Encontrar a URL original pelo hash
+        // Redirecionar para a URL original a partir do que encontramos no DB
+		const { hash } = req.params
+		const url = await URLModel.findOne({ hash })
+
+		if (url) {
+			response.redirect(url.originURL)
+			return
+		}
+
+		response.status(400).json({ error: 'URL not found' })
+	}
+}
